629ebb6784a5fc9995076423a4ec7214c59dfc72
Eric Burkholder <erburk@microsoft.com>
2017-07-26 12:32:28 -0700

    Firt commit of rest endpoint

---
+++ EPUEndpoint/EPluribusUnumEndpoint/Controllers/ValuesController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+
+namespace EPluribusUnumEndpoint.Controllers
+{
+    using Models;
+    public class ValuesController : ApiController
+    {
+        //private workItem aWorkItem; //  ePluribusUnum work Item
+
+        // GET api/values
+        public async Task<ActionResult> Get()
+        {
+
+
+            var items = await DocumentDBRepository<WorkItem>.GetItemsAsync(d => !d.Completed);
+
+            string _JsonStr = JsonConvert.SerializeObject(items);
+                JsonResult _itemsJson = new JsonResult();
+                _itemsJson.Data = _JsonStr;
+
+                return _itemsJson;
+  
+
+        }
+
+        // GET api/values/5
+        public async Task<ActionResult> Get(string id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            WorkItem _item = await DocumentDBRepository<WorkItem>.GetItemAsync(id);
+            if (_item == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
+
+            string _JsonStr = JsonConvert.SerializeObject(_item);
+            JsonResult _anItemJson = new JsonResult();
+            _anItemJson.Data = _JsonStr;
+
+            return _anItemJson;
+
+            // need to return the item.
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+
+        // PUT api/values/5
+        public void Put(String id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+
+
+}
---
+++ EPUEndpoint/EPluribusUnumEndpoint/Models/workItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Azure.Documents;
+using Newtonsoft.Json;
+
+namespace EPluribusUnumEndpoint.Models
+{
+ 
+    public class WorkItem
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "imageURI")]
+        public string imageURI { get; set; }
+
+        [JsonProperty(PropertyName = "displayQuestion")]
+        public string displayQuestion { get; set; }
+
+        [JsonProperty(PropertyName = "payorAddr")]
+        public string payorAddr { get; set; }
+
+        [JsonProperty(PropertyName = "payeeAddr")]
+        public string payeeAddr { get; set; }
+
+        [JsonProperty(PropertyName = "ethereumTransID")]
+        public string ethereumTransID { get; set; }
+
+        [JsonProperty(PropertyName = "friendlyDisplayName")]
+        public string friendlyDisplayName { get; set; }
+
+        [JsonProperty(PropertyName = "isComplete")]
+        public bool Completed { get; set; }
+
+        [JsonProperty(PropertyName = "answer")]
+        public string Answer { get; set; }
+
+    }
+}
